7266f51938a5ce5bffb514bbef72284f1fc1eb14
kishansoya511 <kishangadhavi511@gmail.com>
2024-05-08 16:38:14 +0530

    intial commit

--- home/views.py
+++ home/views.py
@@ -2,13 +2,19 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("this is my homepage (/)")
+    # return HttpResponse("this is my home page (/)")
+    context = {'name':'harry','course':'Django'}
+    return render(request,'home.html',context)
+
 
 def about(request):
-    return HttpResponse("this is my about page (/about)")
+    # return HttpResponse("this is my about page (/about)")
+    return render(request,'about.html')
 
 def projects(request):
-    return HttpResponse("this is my projects page (/project)")
+    # return HttpResponse("this is my projects page (/project)")
+    return render(request,'projects.html')
 
 def contact(request):
-    return HttpResponse("this is my contact page (/contact)")
+    # return HttpResponse("this is my contact page (/contact)")
+    return render(request,'contact.html')
