f23971f6e37363f39910f87e8fb271228d0f46b9
kishansoya511 <kishangadhavi511@gmail.com>
2024-05-11 14:15:34 +0530

    created database in django

--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,HttpResponse
+from home.models import Contact
 
 # Create your views here.
 def home(request):
@@ -16,5 +17,14 @@ def projects(request):
     return render(request,'projects.html')
 
 def contact(request):
+    if request.method =='POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        desc = request.POST['desc']
+        # print(name,email,phone,desc)
+        ins = Contact(name=name,email=email,phone=phone,desc=desc)
+        ins.save
+        print("data has been written to the db")
     # return HttpResponse("this is my contact page (/contact)")
     return render(request,'contact.html')
