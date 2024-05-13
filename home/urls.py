947b6886899506c946f8e35a2b32f32646f67dfb
kishansoya511 <kishangadhavi511@gmail.com>
2024-05-13 10:24:11 +0530

    admin_customization

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from home import views
 
+
+# Django admin header costumization
+
+admin.site.site_header = "Developer Kishan"
+admin.site.site_title = " Welcome Kishan's Dashboard"
+admin.site.index_title = "Welcome to this Portal "
 urlpatterns = [
     path('', views.home,name='home'),
     path('about', views.about,name='about'),
